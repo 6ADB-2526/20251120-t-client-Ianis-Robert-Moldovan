486a6584f14ecb11c72335a71fc356d7d0b2da37
ianis-robert-moldovan <ianisrobert.moldovan@student.tsaam.be>
2025-11-21 12:12:20 +0100

    v2.0.0

--- script/script.js
+++ script/script.js
@@ -6,15 +6,20 @@ const divReedsgeschenk = document.querySelector("#reedsgeschenk")
 const formAddGeschenk = document.querySelector("#addGeschenk")
 const formVerwijderGeschenk = document.querySelector("#verwijderGeschenk")
 const formAddKind = document.querySelector("#addKind")
+const selectGeschenken = document.querySelector("#geschenken")
+const geschenkenVerwijderen = document.querySelector("#geschenkenVerwijderen")
+const geschenkenlijst = document.querySelector("#geschenkenlijst")
 
 // selecteren van submits uit html
 const btnToevoegenGeschenk = document.querySelector("#btnToevoegenGeschenk")
 const btnWisGeschenk = document.querySelector("#btnWisGeschenk")
 const btnVoegKind = document.querySelector("#btnVoegKind")
 
+let selecteID = 0
+
 // ophalen van externe informatie
 fetch("https://o-apiandclient-render.onrender.com/kinderen")
-    .then(info)
+    .then((info) => info.json())
     .then((kinderen) => {
         kinderen.forEach(kind => {
             const newOption = document.createElement("option")
@@ -25,4 +30,41 @@ fetch("https://o-apiandclient-render.onrender.com/kinderen")
     })
 
 
-// geschenk van het selecteerde 
+// geschenk van het selecteerde kindje
+selectKinderen.addEventListener("change", (e) => {
+    selecteID = e.target.value
+
+    fetch("https://o-apiandclient-render.onrender.com/geschenken" + e.target.value)
+        .then((info) => info.json())
+        .then((cadeau) => {
+            geschenkenlijst.innerHTML = ""
+            const newP = document.createElement("P")
+            newP.innerHTML = cadeau.naam
+            geschenkenlijst.appendChild(newP)
+        })
+})
+
+
+// select geschenk toevoegen
+fetch("https://o-apiandclient-render.onrender.com/geschenken")
+    .then((info) => info.json())
+    .then((cadeautjes) => {
+        cadeautjes.forEach(cadeau => {
+            const newOption = document.createElement("option")
+            newOption.value = cadeau.id
+            newOption.innerHTML = cadeau.naam
+            selectGeschenken.appendChild(newOption)
+        });
+    })
+
+fetch("https://o-apiandclient-render.onrender.com/geschenken")
+    .then((info) => info.json())
+    .then((cadeautjes) => {
+        cadeautjes.forEach(cadeau => {
+            const newOption = document.createElement("option")
+            newOption.value = cadeau.id
+            newOption.innerHTML = cadeau.naam
+            geschenkenVerwijderen.appendChild(newOption)
+        });
+    })
+
